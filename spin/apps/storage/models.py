3edae4bf615731f4c26a175e3f07c4c269bd020d
objectisliper <objectisliper@gmail.com>
2019-12-23 15:00:57 +0200

    init

--- spin/apps/storage/models.py
+++ spin/apps/storage/models.py
@@ -21,11 +21,19 @@ class EncryptedText(models.TextField):
     def from_db_value(self, value, expression, connection):
         __key_private = os.environ.get('PRIVATE_ENCRYPT_KEY', None)
         __key_public = os.environ.get('PUBLIC_ENCRYPT_KEY', None)
-        if __key_private is None:
-            raise ValueError('No ENCRYPT_KEY environment variable found!')
-        if __key_private and value:
+        if __key_private is None and __key_public is None:
+            raise ValueError('No PRIVATE_ENCRYPT_KEY or PUBLIC_ENCRYPT_KEY environment variable found!')
+        if value:
+            value_private = value.split('[split_keys]')[0]
+            value_public = value.split('[split_keys]')[1] if len(value.split('[split_keys]')) > 1 else None
+            if value_private and __key_private is not None:
+                value, __decrypt_key = value_private, __key_private
+            elif value_public and __key_public is not None:
+                value, __decrypt_key = value_public, __key_public
+            else:
+                raise ValueError('Can\'t match decrypt key and value')
             value = base64.b64decode(value)
-            value = decrypt(__key_private, value)
+            value = decrypt(__decrypt_key, value)
             value = value.decode("utf-8")
         return value
 
@@ -36,19 +44,19 @@ class EncryptedText(models.TextField):
         __key_private = os.environ.get('PRIVATE_ENCRYPT_KEY', None)
         __key_public = os.environ.get('PUBLIC_ENCRYPT_KEY', None)
         if __key_private is None:
-            raise ValueError('No ENCRYPT_KEY environment variable found!')
+            raise ValueError('No PRIVATE_ENCRYPT_KEY environment variable found!')
         if value:
+            default_value = value
             if __key_private:
-                value = encrypt(__key_private, value)
+                value = encrypt(__key_private, default_value)
                 value = base64.b64encode(value).decode('utf-8')
             if __key_public is not None:
-                public_value = encrypt(__key_public, value)
+                public_value = encrypt(__key_public, default_value)
                 public_value = base64.b64encode(public_value).decode('utf-8')
                 value += f'[split_keys]{public_value}'
         return value
 
 
-# Create your models here.
 class TimeStampedModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
--- spin/apps/storage/tests.py
+++ spin/apps/storage/tests.py
@@ -13,11 +13,37 @@ class EncryptedUserDataModelTestCase(TestCase):
     encrypt_private_key = 'test123'
     encrypt_public_key = 'test_public'
 
-    def test_encryption(self):
+    def test_encryption_with_private_key(self):
+        os.environ['PRIVATE_ENCRYPT_KEY'] = self.encrypt_private_key
+        created_user = create_one(User, commit=False)
+        created_user.save()
+        EncryptedUserData(name=self.user_name, user=created_user).save()
+
+        self.assertEqual((EncryptedUserData.objects.first()).name, self.user_name)
+
+    def test_encryption_with_both_keys(self):
         os.environ['PRIVATE_ENCRYPT_KEY'] = self.encrypt_private_key
         os.environ['PUBLIC_ENCRYPT_KEY'] = self.encrypt_public_key
         created_user = create_one(User, commit=False)
         created_user.save()
         EncryptedUserData(name=self.user_name, user=created_user).save()
+
+        self.assertEqual((EncryptedUserData.objects.first()).name, self.user_name)
+
+    def test_raise_exception_on_encryption_with_only_public_key(self):
+        os.environ['PUBLIC_ENCRYPT_KEY'] = self.encrypt_public_key
+        os.environ.pop('PRIVATE_ENCRYPT_KEY')
+        created_user = create_one(User, commit=False)
+        created_user.save()
+        with self.assertRaises(ValueError):
+            EncryptedUserData(name=self.user_name, user=created_user).save()
+
+    def test_decryption_with_public_key(self):
+        os.environ['PRIVATE_ENCRYPT_KEY'] = self.encrypt_private_key
+        os.environ['PUBLIC_ENCRYPT_KEY'] = self.encrypt_public_key
+        created_user = create_one(User, commit=False)
+        created_user.save()
+        EncryptedUserData(name=self.user_name, user=created_user).save()
+        os.environ.pop('PRIVATE_ENCRYPT_KEY')
 
         self.assertEqual((EncryptedUserData.objects.first()).name, self.user_name)
